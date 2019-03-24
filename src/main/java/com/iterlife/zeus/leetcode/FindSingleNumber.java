ef160b13b7e8bd2493ae187ce2dbc00837f847cd
LUJIE_LENOVO\Lu Jie <lujie0403@163.com>
2019-03-24 18:05:43 +0800

    add leetcode packages

---
+++ src/main/java/com/iterlife/zeus/leetcode/FindSingleNumber.java
@@ -0,0 +1,75 @@
+/**
+ *
+ * @project iterlife-zeus
+ * @file com.iterlife.zeus.algorithm.FindSingleNumber.java
+ * @version 1.0.0
+ * Copyright 2019 - 2019 for Lu Jie
+ * https://www.iterlife.com
+ *
+ **/
+package com.iterlife.zeus.leetcode;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @desc https://leetcode-cn.com/problems/single-number-iii/
+ *       https://leetcode-cn.com/problems/single-number/
+ *       https://leetcode-cn.com/problems/single-number-ii/
+ * 
+ * @author Lu Jie
+ * @date 2019 2019年3月24日 上午11:47:09
+ * @tags
+ */
+public class FindSingleNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a[] = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+		System.out.println(new FindSingleNumber().singleNumber1(a));;
+
+	}
+
+	public int singleNumber1(int[] nums) {
+		int result = 0;
+		for (int num : nums) {
+			result ^= num;
+		}
+		return result;
+	}
+	public int[] singleNumber2(int[] nums) {
+		List<Integer> list = new LinkedList<Integer>();
+		for (int i = 0; i < nums.length; ++i) {
+			int temp = nums[i];
+			boolean removed = list.removeIf(e -> e.equals(temp));
+			if (!removed) {
+				list.add(temp);
+			}
+		}
+		return list.stream().mapToInt(Integer::intValue).toArray();
+	}
+
+	public int singleNumber3(int[] nums) {
+		int result = 0;
+		Map<Integer, Integer> items = new HashMap<Integer, Integer>();
+		for (int num : nums) {
+			Integer intValue = (items.get(num) == null)
+					? 1
+					: items.get(num) + 1;
+			items.put(num, intValue);
+		}
+
+		for (Map.Entry<Integer, Integer> kv : items.entrySet()) {
+			if (kv.getValue() == 1) {
+				result = kv.getKey();
+				break;
+			}
+		}
+		return result;
+	}
+}
---
+++ src/main/java/com/iterlife/zeus/leetcode/SortArrayByParity.java
@@ -0,0 +1,48 @@
+/**
+ *
+ * @project iterlife-zeus
+ * @file com.iterlife.zeus.leetcode.OddEvenNumber.java
+ * @version 1.0.0
+ * Copyright 2019 - 2019 for Lu Jie
+ * https://www.iterlife.com
+ *
+ **/
+package com.iterlife.zeus.leetcode;
+
+/**
+ *
+ * @desc https://leetcode-cn.com/problems/sort-array-by-parity/
+ * @author Lu Jie
+ * @date 2019 2019年3月24日 下午3:16:47
+ * @tags
+ */
+public class SortArrayByParity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a[] = {0, 2};
+		new SortArrayByParity().sortArrayByParity(a);
+		for (int k : a) {
+			System.out.print(k + " ");
+		}
+	}
+
+	public int[] sortArrayByParity(int[] A) {
+
+		for (int i = 0, j = A.length - 1; i < j; ++i, --j) {
+			while (i < j && A[i] % 2 == 0) {
+				i++;
+			}
+			while (j > i && A[j] % 2 == 1) {
+				j--;
+			}
+			int temp = A[i];
+			A[i] = A[j];
+			A[j] = temp;
+		}
+		return A;
+	}
+
+}
