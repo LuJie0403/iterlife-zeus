2048b0c6e5cc50ed7df462d87020b027bd8786d2
iter-1024 <lujie0403@163.com>
2020-10-07 14:55:37 +0800

    update formate

--- src/main/java/com/iterlife/zeus/Application.java
+++ src/main/java/com/iterlife/zeus/Application.java
@@ -3,26 +3,26 @@ package com.iterlife.zeus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ComponentScan.Filter;
 
 /**
- *
- * @desc Hello World
  * @author Lu Jie
+ * @desc Hello World
  * @date 2019 2019年2月13日 上午11:43:51
  * @tags
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @Configuration
 @ComponentScan(basePackages = {
-		"com.iterlife.zeus" }, useDefaultFilters = false, includeFilters = @Filter(type = FilterType.ANNOTATION))
+        "com.iterlife.zeus"}, useDefaultFilters = false, includeFilters = @Filter(type = FilterType.ANNOTATION))
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+    }
 
 }
--- src/main/java/com/iterlife/zeus/entity/User.java
+++ src/main/java/com/iterlife/zeus/entity/User.java
@@ -1,14 +1,42 @@
 package com.iterlife.zeus.entity;
 
-import lombok.*;
 
-@Data
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class User {
-
     private String id;
 
     private String name;
 
     public int age;
+
+    public User(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
 }
