96fe4fb316150c58f9dcbec09746efce8bd2434d
lujie <lujie0403@gmail.com>
2021-05-11 18:55:49 +0800

    https://leetcode-cn.com/problems/xor-operation-in-an-array/

---
+++ zeus-leetcode/src/main/java/com/iterlife/zeus/leetcode/XorOperationInAnArray.java
@@ -0,0 +1,23 @@
+package com.iterlife.zeus.leetcode;
+
+/**
+ * @desc:https://leetcode-cn.com/problems/xor-operation-in-an-array/
+ * @author: lujie
+ * @version: V1.0.0
+ * @datetime: 2021/5/11 18:44
+ **/
+public class XorOperationInAnArray {
+    public static void main(String[] args) {
+        System.out.print(new XorOperationInAnArraySolution().xorOperation(1, 0));
+    }
+}
+
+class XorOperationInAnArraySolution {
+    public int xorOperation(int n, int start) {
+        int result = start;
+        for (int i = 1; i < n; ++i) {
+            result ^= (2 * i + start);
+        }
+        return result;
+    }
+}
