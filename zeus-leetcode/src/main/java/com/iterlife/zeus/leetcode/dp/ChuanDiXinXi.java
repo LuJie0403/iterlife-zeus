2799a1d463c7e948d3e91a20402420e36ae34a2d
lujie <lujie0403@gmail.com>
2021-08-23 00:23:51 +0800

    https://leetcode-cn.com/problems/chuan-di-xin-xi/

--- zeus-leetcode/src/main/java/com/iterlife/zeus/leetcode/dp/ChuanDiXinXi.java
+++ zeus-leetcode/src/main/java/com/iterlife/zeus/leetcode/dp/ChuanDiXinXi.java
@@ -12,14 +12,12 @@ public class ChuanDiXinXi {
     public static void main(String args[]) {
         int[][] relation = new int[][]{{0, 2}, {2, 1}, {3, 4}, {2, 3}, {1, 4}, {2, 0}, {0, 4}};
         System.out.println(new ChuanDiXinXiSolution().numWays1(5, relation, 3));
+        System.out.println(new ChuanDiXinXiSolution().numWays2(5, relation, 3));
     }
 }
 
 class ChuanDiXinXiSolution {
     public int numWays1(int n, int[][] relation, int k) {
-
-        printMap(n, relation);
-
         //dp[i][j]表示第j轮传递到编号为i的小朋友的总方案数
         //dp[i][j]+=dp[i][j-1]
         int[][] dp = new int[n][k + 1];
@@ -30,53 +28,25 @@ class ChuanDiXinXiSolution {
                 int v = relation[j][1];
                 dp[v][i] += dp[u][i - 1];
             }
-            printDp(i, dp);
         }
         return dp[n - 1][k];
     }
 
-    public void printDp(int k, int[][] input) {
-        int[][] output = new int[input[0].length][input.length];
-        for (int i = 0; i < output.length; i++) {
-            for (int j = 0; j < output[i].length; ++j) {
-                output[i][j] = input[j][i];
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/chuan-di-xin-xi/solution/chuan-di-xin-xi-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public int numWays2(int n, int[][] relation, int k) {
+        int[][] dp = new int[k + 1][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < k; i++) {
+            for (int[] edge : relation) {
+                int src = edge[0], dst = edge[1];
+                dp[i + 1][dst] += dp[i][src];
             }
         }
-
-
-        System.out.println(String.format("\n第 %s 轮动态规划结果:", k));
-        for (int i = 0; i < k; ++i) {
-            for (int j = 0; j < output[i].length; ++j) {
-                System.out.print(String.format(" %s  |", output[i][j]));
-            }
-            System.out.println("\n");
-        }
-        for (int i = 0; i < k; ++i) {
-            for (int j = 0; j < output[i].length; ++j) {
-                if (i == k - 1 && output[i][j] > 0) {
-                    System.out.println(String.format("第 %s 轮结束后到达 %s 的路径长度畅通，距离+%s", i, j, output[i][j]));
-                }
-            }
-        }
-    }
-
-    public void printMap(int k, int[][] input) {
-        int[][] map = new int[k][k];
-
-        for (int i = 0; i < input.length; ++i) {
-            map[input[i][0]][input[i][1]] = 1;
-        }
-        System.out.print("   ||");
-        for (int i = 0; i < map.length; ++i) {
-            System.out.print(String.format("  %s |", i));
-        }
-        System.out.println("\n________________________________");
-        for (int i = 0; i < map.length; ++i) {
-            System.out.print(String.format(" %s ||", i));
-            for (int j = 0; j < map[i].length; ++j) {
-                System.out.print(String.format(" %s  |", map[i][j] == 1 ? " " : "0"));
-            }
-            System.out.print("\n");
-        }
+        return dp[k][n - 1];
     }
 }
