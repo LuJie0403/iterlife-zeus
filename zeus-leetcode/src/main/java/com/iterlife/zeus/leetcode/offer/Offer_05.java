f29dc2d25690d8502597d051a3196a0c66ee30e1
lujie <lujie0403@gmail.com>
2021-08-30 22:38:12 +0800

    https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/

--- zeus-leetcode/src/main/java/com/iterlife/zeus/leetcode/offer/Offer_05.java
+++ zeus-leetcode/src/main/java/com/iterlife/zeus/leetcode/offer/Offer_05.java
@@ -12,6 +12,16 @@ public class Offer_05 {
 
 class Offer_05_Solution {
     public String replaceSpace(String s) {
-        return s.replace(" ", "%20");
+        // return s.replace(" ", "%20");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 }
