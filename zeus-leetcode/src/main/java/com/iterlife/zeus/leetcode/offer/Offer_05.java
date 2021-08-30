812d51e5117bcd7ee6ac3a05d90c9efacd6b7d8b
lujie <lujie0403@gmail.com>
2021-08-30 22:34:46 +0800

    https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/

---
+++ zeus-leetcode/src/main/java/com/iterlife/zeus/leetcode/offer/Offer_05.java
@@ -0,0 +1,17 @@
+package com.iterlife.zeus.leetcode.offer;
+
+/**
+ * @desc:https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
+ * @author: lujie
+ * @version: V1.0.0
+ * @datetime: 2021/8/30 22:31
+ **/
+public class Offer_05 {
+}
+
+
+class Offer_05_Solution {
+    public String replaceSpace(String s) {
+        return s.replace(" ", "%20");
+    }
+}
