dd7fdfe34f159b15b1d9f2436f7a31f4283a31f0
lujie <lujie0403@gmail.com>
2021-09-02 23:58:17 +0800

    https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/

--- zeus-leetcode/src/main/java/com/iterlife/zeus/leetcode/offer/Offer_25.java
+++ zeus-leetcode/src/main/java/com/iterlife/zeus/leetcode/offer/Offer_25.java
@@ -38,7 +38,7 @@ class Offer_25_Solution {
     /**
      * l2 拼接到 l1 实现合并排序
      */
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
         }
@@ -76,5 +76,27 @@ class Offer_25_Solution {
             temp2.next = l2;
         }
         return result;
+    }
+
+
+    /**
+     * 递归方法
+     */
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        }
+        if (l1.val > l2.val) {
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
+        return null;
     }
 }
