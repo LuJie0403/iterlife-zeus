35839005dcf7376ce0d6cb43d01954943bc13b19
lujie <lujie0403@gmail.com>
2021-09-03 09:18:53 +0800

    https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/

--- zeus-leetcode/src/main/java/com/iterlife/zeus/leetcode/offer/Offer_18.java
+++ zeus-leetcode/src/main/java/com/iterlife/zeus/leetcode/offer/Offer_18.java
@@ -9,7 +9,7 @@ package com.iterlife.zeus.leetcode.offer;
 public class Offer_18 {
     public static void main(String args[]) {
         Offer_18_Solution.ListNode l1 = new Offer_18_Solution.ListNode(4, new Offer_18_Solution.ListNode(5, new Offer_18_Solution.ListNode(1, new Offer_18_Solution.ListNode(9, null))));
-        new Offer_18_Solution().deleteNode(l1, 5);
+        new Offer_18_Solution().deleteNode1(l1, 5);
     }
 }
 
@@ -32,7 +32,10 @@ class Offer_18_Solution {
         }
     }
 
-    public ListNode deleteNode(ListNode head, int val) {
+    /**
+     * 使用两个指针
+     */
+    public ListNode deleteNode2(ListNode head, int val) {
         if (head == null) {
             return null;
         }
@@ -48,6 +51,28 @@ class Offer_18_Solution {
                 break;
             }
             preNode = pointer;
+            pointer = pointer.next;
+        }
+        return head;
+    }
+
+    /**
+     * 使用一个指针
+     */
+    public ListNode deleteNode1(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        if (head.val == val) {
+            return head.next;
+        }
+        ListNode pointer = head;
+        while (pointer.next != null) {
+            if (pointer.next.val == val) {
+                pointer.next = pointer.next.next;
+                pointer = pointer.next;
+                break;
+            }
             pointer = pointer.next;
         }
         return head;
