1d1be3e5fab3dec0e9e1844475fa6c7b8d4afe01
lujie <lujie0403@gmail.com>
2021-09-04 14:39:38 +0800

    https://leetcode-cn.com/problems/intersection-of-two-linked-lists/

--- zeus-leetcode/src/main/java/com/iterlife/zeus/leetcode/offer/Offer_52.java
+++ zeus-leetcode/src/main/java/com/iterlife/zeus/leetcode/offer/Offer_52.java
@@ -1,7 +1,7 @@
 package com.iterlife.zeus.leetcode.offer;
 
 /**
- * @desc:
+ * @desc:https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
  * @author: lujie
  * @version: V1.0.0
  * @datetime: 2021/9/3 14:02
@@ -24,32 +24,35 @@ class Offer_52_Solution {
         }
 
         /**
-         * 两个链表不一样长，则
+         * 两个链表A和B相交，则 A+B 和 B+A 长度一致，则分别遍历一遍，同时到达结尾的位置
+         * 如果 A 和 B 的长度差为 size,则分别循环反复遍历 A+B 和 B+A size 次数，可以同时变量到 A 和 B 相差的位置
          */
         public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
             if (headA == null || headB == null) {
                 return null;
             }
-            ListNode nodeA = headA;
-            ListNode nodeB = headB;
-            int lengthA = getListNodeLength(nodeA);
-            int lengthB = getListNodeLength(nodeA);
-           
-
-
-            return null;
+            ListNode pNodeA = headA;
+            ListNode pNodeB = headB;
+            int counter = 0;
+            while (pNodeA != pNodeB) {
+                pNodeA = (pNodeA == null ? headB : pNodeA.next);
+                pNodeB = (pNodeB == null ? headA : pNodeB.next);
+                counter++;
+                print(counter, "A", pNodeA);
+                print(counter, "B", pNodeB);
+            }
+            System.out.println(String.format(" counter=%s", counter));
+            return pNodeA;
         }
 
-        private int getListNodeLength(ListNode node) {
-            if (node == null) {
-                return 0;
-            }
-            int length = 0;
+
+        private void print(int index, String listNodeName, ListNode node) {
+            System.out.print(String.format("%d:%s=[", index, listNodeName));
             while (node != null) {
-                length++;
+                System.out.print(String.format(" %s ", node.val));
                 node = node.next;
             }
-            return length;
+            System.out.println("]");
         }
     }
 }
