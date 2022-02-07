56a8a18993f50ded89844bc4f96f24e6b171f2a0
路杰 <lujie@lattebank.com>
2022-02-07 21:55:16 +0800

    代码结构化改造与可读性完善

--- zeus-tqi/src/main/java/com/iterlife/zeus/tqi/XData.java
+++ zeus-tqi/src/main/java/com/iterlife/zeus/tqi/XData.java
@@ -18,26 +18,32 @@ import java.util.LinkedList;
 public class XData {
 
     //项数量，默认等于7
-    public int size;
+    public int itemCapacity;
+    public int mileageCounter;
+    public double[] sumValue;
+    public double[] avgValue;
     public LinkedList[] xElemList;
 
 
-    public XData(int size) {
-        this.size = size;
-        this.xElemList = new LinkedList[this.size];
-        for (int i = 0; i < size; ++i) {
+    public XData(int itemCapacity) {
+        this.itemCapacity = itemCapacity;
+        this.mileageCounter = 0;
+        this.xElemList = new LinkedList[this.itemCapacity];
+        for (int i = 0; i < itemCapacity; ++i) {
+            this.sumValue[i] = 0.0d;
+            this.avgValue[i] = 0.0d;
             this.xElemList[i] = new LinkedList();
         }
     }
 
 
-    public void addData(int i, int j, double value) {
-        XElem preXElem = getPreXElem(i);
-        XElem xElem = new XElem(j, value, preXElem.getSumValue(), preXElem.getAccumulatedSdamValue());
-        xElemList[i].addLast(xElem);
+    public void appendXElem(int itemIndex, int j, double value) {
+        XElem lastXElem = getLastXElem(itemIndex);
+        XElem xElem = new XElem(j, value, lastXElem.getSumValue(), lastXElem.getAccumulatedSdamValue());
+        xElemList[itemIndex].addLast(xElem);
     }
 
-    private XElem getPreXElem(int i) {
+    private XElem getLastXElem(int i) {
         LinkedList<XElem> xElemList = this.xElemList[i];
         if (xElemList.isEmpty()) {
             return new XElem(0, 0.0d, 0.0d, 0.0d, 0.0d, 0.0d, 0.0d);
