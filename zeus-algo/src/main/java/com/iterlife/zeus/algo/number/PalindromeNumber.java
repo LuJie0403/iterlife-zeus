009a28d83b8d591dbd8e46ee31eb29a50092b3a2
lujie <lujie@lattebank.com>
2024-04-25 13:39:54 +0800

    AC: https://leetcode.cn/problems/palindrome-number/

---
+++ zeus-algo/src/main/java/com/iterlife/zeus/algo/number/PalindromeNumber.java
@@ -0,0 +1,36 @@
+package com.iterlife.zeus.algo.number;
+
+/**
+ * @desc:https://leetcode.cn/problems/palindrome-number/
+ * @author: lujie
+ * @version: V1.0.0
+ * @datetime: 2024/4/24 20:05
+ **/
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        System.out.println(new PalindromeNumberSolution().isPalindrome(10));
+
+    }
+}
+
+class PalindromeNumberSolution {
+
+    public boolean isPalindrome(int x) {
+
+        int result = 0;
+        int help = 10;
+        if (x < 0 || (x % help == 0 && x != 0)) {
+            return false;
+        }
+        while (x > result) {
+            System.out.println("Before:x=" + x + ",result=" + result);
+            result = result * help + x % help;
+            if (x == result) {
+                return true;
+            }
+            x /= help;
+            System.out.println("After:x=" + x + ",result=" + result);
+        }
+        return x == result;
+    }
+}
